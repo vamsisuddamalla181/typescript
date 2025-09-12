60260dfafaca3035d91bb484f2722737f28b6594
vamsisuddamalla181 <vamsikrishnanaidu.suddamalla@tekreant.com>
2025-09-12 21:34:26 +0530

    import export modules are used

---
+++ export.ts
@@ -0,0 +1,14 @@
+export function add(a:number,b:number){
+    return a+b
+}
+export class Calculator {
+    multiply(a: number, b: number) {
+        return a * b;
+    }
+}
+export const c= (a:number,b:number)=>{
+    return a**b
+}
+export default function name(a:string){
+    return `this is my name ${a}`
+}
---
+++ import.ts
@@ -0,0 +1,6 @@
+import name,{add,Calculator,c} from "./export"
+console.log(add(10,12))
+const calc=new Calculator()
+console.log(calc.multiply(2,3))
+console.log(c(2,3))
+console.log(name("vamsi"))
